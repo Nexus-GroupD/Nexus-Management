d112655cfe6244683520f6ef6caeef0e06cd6416
J0shuaMarcellus <Joshuar.marcellus@gmail.com>
2026-03-23 09:30:16 -0500

    changes to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -52,7 +52,7 @@ const Navbar: React.FC<NavbarProps> = ({ pageTitle = 'Nexus Management' }) => {
         </button>
 
         <h1 className="navbar-title">
-          <span className="navbar-logo">N</span>
+          <span className="navbar-logo">N </span>
           {pageTitle}
         </h1>
 
